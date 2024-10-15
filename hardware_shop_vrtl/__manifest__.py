0dbf16097b2bc92a2690823b2e3c297aa5b9a1e8
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-10-15 14:29:48 +0200

    Update __manifest__.py

--- hardware_shop_vrtl/__manifest__.py
+++ hardware_shop_vrtl/__manifest__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Industries: Hardware shop, Vertel',
-    'version': '14.0.0.1.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '1.0',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Hardware shop, Vertel',
     'category': 'Industries',
     'description': """
