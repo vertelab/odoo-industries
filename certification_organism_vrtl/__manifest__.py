aeee0371a1d514f30cd2d03b2e76fb7c51d17bdb
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-10-15 14:27:42 +0200

    Update __manifest__.py

--- certification_organism_vrtl/__manifest__.py
+++ certification_organism_vrtl/__manifest__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Industries: Certification Auditors, Vertel',
-    'version': '14.0.0.1.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '1.0',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Certification Auditors, Vertel',
     'category': 'Industries',
     'description': """
