a7b615371e7be7f64246c5b589d61522c650a8a4
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-10-15 14:25:13 +0200

    Update __manifest__.py

--- art_craft_vrtl/__manifest__.py
+++ art_craft_vrtl/__manifest__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Industries: The Arts & Craft Shop, Vertel',
-    'version': '14.0.0.1.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '1.0',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'The Arts & Craft Shop, Vertel',
     'category': 'Industries',
     'description': """
