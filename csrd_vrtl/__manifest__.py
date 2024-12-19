996f466cc26b789aee4738ddbf74a0770e6a06c6
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-12-19 11:52:09 +0100

    Update __manifest__.py

--- csrd_vrtl/__manifest__.py
+++ csrd_vrtl/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Industries: CSRD, Vertel',
-    'version': '1.0',
+    'version': '1.0.0',
     # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'CSRD, a collection of modules, Vertel',
     'category': 'Industries',
