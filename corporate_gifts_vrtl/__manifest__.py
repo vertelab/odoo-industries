e5a70b5c2995ca13720d8490072889e97a19f736
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-12-19 11:51:49 +0100

    Update __manifest__.py

--- corporate_gifts_vrtl/__manifest__.py
+++ corporate_gifts_vrtl/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Industries: Corporate Gifts, Vertel',
-    'version': '14.0.0.1.0',
+    'version': '1.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Corporate Gifts, Vertel',
     'category': 'Industries',
