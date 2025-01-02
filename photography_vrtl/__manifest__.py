3906e2f5f9b005918916182aedfb8827f58982fc
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-01-02 15:50:09 +0100

    Update __manifest__.py

--- photography_vrtl/__manifest__.py
+++ photography_vrtl/__manifest__.py
@@ -33,7 +33,7 @@
     """,
     #'sequence': '1'
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/odoo-industries/l10n_se_payroll',
+    'website': 'https://vertel.se/apps/odoo-industries/photography_vrtl',
     'images': ['static/description/banner.png'], # 560x280 px.
     #'license': 'AGPL-3',
     'contributor': '',
