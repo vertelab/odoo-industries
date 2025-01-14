4118bafda1001db5064738bfa5c1fc3f544b1391
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-01-14 14:12:20 +0100

    Update __manifest__.py

--- surveyor_vrtl/__manifest__.py
+++ surveyor_vrtl/__manifest__.py
@@ -33,7 +33,7 @@
     """,
     #'sequence': '1'
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/odoo-industries/l10n_se_payroll',
+    'website': 'https://vertel.se/apps/odoo-industries/surveyor_vrtl',
     'images': ['static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
     'contributor': '',
